85d564c77a84a8ae46cea1b37d1058cbec113f40
Surbhi Sharma <ssurbhi560@gmail.com>
2022-02-14 06:06:05 +0530

    add ipython in flask shell

--- frenzy.py
+++ frenzy.py
@@ -1,1 +1,14 @@
-from app import app
+from app import app, db
+from app.models import Dish, PurchaseOrder, Restaurant, Schedule, User
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        "db": db,
+        "Restaurant": Restaurant,
+        "Schedule": Schedule,
+        "Dish": Dish,
+        "User": User,
+        "PurchaseOrder": PurchaseOrder,
+    }

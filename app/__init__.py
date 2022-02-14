3382082d017d1b96e735e620bf4f3d4fde01c272
Surbhi Sharma <ssurbhi560@gmail.com>
2022-02-14 06:14:14 +0530

    add 'flask seed-db' command for data loading

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,7 @@
+import os
+
+import click
+import requests
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -10,4 +14,17 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-from app import models, routes
+from app import load, models, routes
+
+
+@app.cli.command("seed-db", help="Seed the database with initial data.")
+def seed_db():
+    restaurant_data_url = os.getenv("RESTAURANT_DATA_URL")
+    users_data_url = os.getenv("USER_DATA_URL")
+    restaurants = requests.get(restaurant_data_url).json()
+    users = requests.get(users_data_url).json()
+
+    click.echo("Adding data to database...")
+    load.add_restaurants(restaurants)
+    load.add_users(users)
+    click.echo("Done.")

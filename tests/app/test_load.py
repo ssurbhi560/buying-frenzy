ab1bc5d7d707bf56ab37f75203ae574276d2e441
Surbhi Sharma <ssurbhi560@gmail.com>
2022-02-16 22:35:21 +0530

    Add more tests for loading script

--- tests/app/test_load.py
+++ tests/app/test_load.py
@@ -44,6 +44,122 @@ def test_parse_schedule():
 
     assert parse_schedule(hours) == expected
 
+    hours = (
+        "Sun - Mon 11:45 am - 1 pm / "
+        "Tues 4:30 pm - 9:15 pm / "
+        "Weds, Sat 5 pm - 7:45 pm / "
+        "Thurs 10:45 am - 1:15 am / "
+        "Fri 10:30 am - 9:30 pm"
+    )
+    expected = {
+        0: (t(11, 45), t(13, 0), False),
+        1: (t(16, 30), t(21, 15), False),
+        2: (t(17, 0), t(19, 45), False),
+        3: (t(10, 45), t(1, 15), True),
+        4: (t(10, 30), t(21, 30), False),
+        5: (t(17, 0), t(19, 45), False),
+        6: (t(11, 45), t(13, 0), False),
+    }
+    assert parse_schedule(hours) == expected
+
+    hours = (
+        "Sun - Tues, Thurs - Fri 10:15 am - 9 pm / "
+        "Weds 12:30 pm - 3:30 pm / "
+        "Sat 10 am - 7:30 pm"
+    )
+    expected = {
+        0: (t(10, 15), t(21, 0), False),
+        1: (t(10, 15), t(21, 0), False),
+        2: (t(12, 30), t(15, 30), False),
+        3: (t(10, 15), t(21, 0), False),
+        4: (t(10, 15), t(21, 0), False),
+        5: (t(10, 0), t(19, 30), False),
+        6: (t(10, 15), t(21, 0), False),
+    }
+    assert parse_schedule(hours) == expected
+
+    hours = (
+        "Mon - Tues 6:45 am - 8:45 am / "
+        "Weds 11:15 am - 2 pm / "
+        "Thurs 5:30 pm - 2 am / "
+        "Fri 12:15 pm - 3:45 am / "
+        "Sat 2:30 pm - 8:15 pm / "
+        "Sun 8:15 am - 7:15 pm"
+    )
+    expected = {
+        0: (t(6, 45), t(8, 45), False),
+        1: (t(6, 45), t(8, 45), False),
+        2: (t(11, 15), t(14, 0), False),
+        3: (t(17, 30), t(2, 0), True),
+        4: (t(12, 15), t(3, 45), True),
+        5: (t(14, 30), t(20, 15), False),
+        6: (t(8, 15), t(19, 15), False),
+    }
+    assert parse_schedule(hours) == expected
+
+    hours = "Mon, Wed-Sun 11 am - 10 pm"
+    expected = {
+        0: (t(11, 00), t(22, 00), False),
+        2: (t(11, 00), t(22, 00), False),
+        3: (t(11, 00), t(22, 00), False),
+        4: (t(11, 00), t(22, 00), False),
+        5: (t(11, 00), t(22, 00), False),
+        6: (t(11, 00), t(22, 00), False),
+    }
+    assert parse_schedule(hours) == expected
+
+    hours = (
+        "Mon 11 am - 12:45 am / "
+        "Tues - Weds 8:30 am - 12:45 am / "
+        "Thurs 5:45 pm - 1 am / "
+        "Fri 5 am - 8:15 pm / "
+        "Sat 1:15 pm - 2:30 pm / "
+        "Sun 3:30 pm - 6:30 pm"
+    )
+    expected = {
+        0: (t(11, 0), t(0, 45), True),
+        1: (t(8, 30), t(0, 45), True),
+        2: (t(8, 30), t(0, 45), True),
+        3: (t(17, 45), t(1, 0), True),
+        4: (t(5, 0), t(20, 15), False),
+        5: (t(13, 15), t(14, 30), False),
+        6: (t(15, 30), t(18, 30), False),
+    }
+    assert parse_schedule(hours) == expected
+
+    hours = (
+        "Mon - Tues, Thurs 11:45 am - 1:30 pm / "
+        "Weds, Sat 12 pm - 7:45 pm / "
+        "Fri 7 am - 9:30 am / "
+        "Sun 10:15 am - 10:30 pm"
+    )
+    expected = {
+        0: (t(11, 45), t(13, 30), False),
+        1: (t(11, 45), t(13, 30), False),
+        2: (t(12, 0), t(19, 45), False),
+        3: (t(11, 45), t(13, 30), False),
+        4: (t(7, 0), t(9, 30), False),
+        5: (t(12, 0), t(19, 45), False),
+        6: (t(10, 15), t(22, 30), False),
+    }
+    assert parse_schedule(hours) == expected
+
+    hours = (
+        "Sun - Tues, Thurs - Fri 10:15 am - 9 pm / "
+        "Weds 12:30 pm - 3:30 pm / "
+        "Sat 10 am - 7:30 pm"
+    )
+    expected = {
+        0: (t(10, 15), t(21, 0), False),
+        1: (t(10, 15), t(21, 0), False),
+        2: (t(12, 30), t(15, 30), False),
+        3: (t(10, 15), t(21, 0), False),
+        4: (t(10, 15), t(21, 0), False),
+        5: (t(10, 0), t(19, 30), False),
+        6: (t(10, 15), t(21, 0), False),
+    }
+    assert parse_schedule(hours) == expected
+
 
 def test_add_restaurant(db):
     data = [

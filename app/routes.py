20395b1bd205262af280bf51dc40c0e63d35dd54
Surbhi Sharma <ssurbhi560@gmail.com>
2022-02-16 22:27:08 +0530

    Remove test route

--- app/routes.py
+++ app/routes.py
@@ -4,11 +4,6 @@ from app import app
 from app.schema import SCHEMA
 
 
-@app.route("/")
-def home():
-    return "Backend Service for a food delivery platform."
-
-
 app.add_url_rule(
     "/graphql",
     view_func=GraphQLView.as_view("graphql", schema=SCHEMA, graphiql=True),
